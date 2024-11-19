78c01574e34cf9b9900e3a08492e1b6bf5dce74a
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2024-11-20 03:35:47 +0800

    asdasd
    asdasdasdas

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
-
+import 'package:pusher_channels_flutter/pusher_channels_flutter.dart'; // Import Pusher package
 import 'notificationpage.dart';
 import 'devicespage.dart';
 import 'homepage.dart';
  
-
-void main()async {
+import 'package:shared_preferences/shared_preferences.dart';
+ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-    // late PusherChannelsFlutter Pusher;
- 
+  
+  // Initialize SharedPreferences to store notifications
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  await onConnectPressed(prefs);
+
   runApp(MyApp());
 }
 
@@ -33,7 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
   static List<Widget> _widgetOptions = <Widget>[
     HomePage(),
     const DevicesPage(),
-      NotificationPage(),
+    NotificationPage(),
   ];
 
   void _onItemTapped(int index) {
@@ -69,4 +72,55 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+}
+
+Future<void> onConnectPressed(SharedPreferences prefs) async {
+  PusherChannelsFlutter pusher = PusherChannelsFlutter.getInstance();
+
+  try {
+    await pusher.init(
+      apiKey: "3ef10ab69edd1c712eeb",
+      cluster: "ap1",
+      onConnectionStateChange: (currentState, previousState) {
+        print("Connection: $currentState");
+      },
+      onError: (message, code, e) {
+        print("onError: $message code: $code exception: $e");
+      },
+      onSubscriptionSucceeded: (channelName, data) {
+        print("onSubscriptionSucceeded: $channelName data: $data");
+      },
+      onEvent: (event) {
+        print("onEvent: $event");
+        // Store notifications in SharedPreferences
+        storeNotification(event, prefs);
+      },
+      onSubscriptionError: (message, e) {
+        print("onSubscriptionError: $message Exception: $e");
+      },
+      onDecryptionFailure: (event, reason) {
+        print("onDecryptionFailure: $event reason: $reason");
+      },
+      onMemberAdded: (channelName, member) {
+        print("onMemberAdded: $channelName user: $member");
+      },
+      onMemberRemoved: (channelName, member) {
+        print("onMemberRemoved: $channelName user: $member");
+      },
+      onSubscriptionCount: (channelName, subscriptionCount) {
+        print("onSubscriptionCount: $channelName subscriptionCount: $subscriptionCount");
+      },
+    );
+    await pusher.subscribe(channelName: "test");
+    await pusher.connect();
+  } catch (e) {
+    print("ERROR: $e");
+  }
+}
+
+void storeNotification(PusherEvent event, SharedPreferences prefs) {
+  List<String> notifications = prefs.getStringList('notifications') ?? [];
+  String notificationMessage = event.data.toString(); // Save event data
+  notifications.add(notificationMessage);
+  prefs.setStringList('notifications', notifications); // Store the notifications list in SharedPreferences
 }
--- lib/notificationpage.dart
+++ lib/notificationpage.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
- 
+import 'package:shared_preferences/shared_preferences.dart';
+
 class NotificationPage extends StatefulWidget {
   @override
   _NotificationPageState createState() => _NotificationPageState();
 }
 
 class _NotificationPageState extends State<NotificationPage> {
-  List<String> notifications = [];
-   @override
+  List<String> notifications = []; // List to store notifications
+
+  @override
   void initState() {
     super.initState();
-   }
+    loadNotifications(); // Load notifications from SharedPreferences
+  }
 
- 
+  // Load stored notifications from SharedPreferences
+  void loadNotifications() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      notifications = prefs.getStringList('notifications') ?? [];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +33,7 @@ class _NotificationPageState extends State<NotificationPage> {
         itemCount: notifications.length,
         itemBuilder: (context, index) {
           return ListTile(
-            title: Text(notifications[index]),
+            title: Text(notifications[index]), // Display notification message
           );
         },
       ),
